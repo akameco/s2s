86d6a0a9a8accd8f1aa13fd7e7078c10ad4b853d
akameco <akameco.t@gmail.com>
2017-09-22 08:49:38 +0900

    Add hook test

---
+++ src/hooks/__tests__/run.test.js
@@ -0,0 +1,18 @@
+// @flow
+import runHooks from '../run'
+import prettierHook from '../prettier'
+
+test('call hooks', () => {
+  const mockHook = jest.fn()
+  const hooks = [mockHook]
+  runHooks('', '', hooks)
+  runHooks('', '', hooks)
+  expect(mockHook.mock.calls.length).toBe(2)
+})
+
+test('return formated code when run prettierHook', () => {
+  const hooks = [prettierHook()]
+  const code = `const      hello =    'world'`
+  const expected = `const hello = 'world'`
+  expect(runHooks('', code, hooks).trim()).toEqual(expected)
+})
