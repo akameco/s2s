7c084891802cb8d0ecccd5fd77d2277ff0189263
akameco <akameco.t@gmail.com>
2017-09-22 09:03:40 +0900

    Add more test to reporters

--- src/reporters/__tests__/index.test.js
+++ src/reporters/__tests__/index.test.js
@@ -5,9 +5,16 @@ test('trimAndFormatPath', () => {
   expect(trimAndFormatPath('/src/user/hoge/index.js')).toMatchSnapshot()
 })
 
-test('formatText', () => {
+test('formatText when handlerType = s2s', () => {
   const inputPath = 'src/input/input.js'
   const outputPath = 'src/output/output.js'
   const result = formatText('S2S', inputPath, outputPath)
+  expect(result).toMatchSnapshot()
+})
+
+test('formatText when handlerType = template', () => {
+  const inputPath = 'src/input/input.js'
+  const outputPath = 'src/output/output.js'
+  const result = formatText('TEMPLATE', inputPath, outputPath)
   expect(result).toMatchSnapshot()
 })
