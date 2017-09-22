f3f9ee828e3f14d368a6b0990e350ee4a9b8b410
akameco <akameco.t@gmail.com>
2017-09-22 21:14:36 +0900

    Refactor test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,16 +3,31 @@ import m from '../index'
 import type { Opts } from '../types'
 import * as utils from '../utils'
 
-const mock = jest.spyOn(utils, 'log')
-mock.mockImplementation(x => x)
-
+let mock
 const watchers = []
 
+beforeEach(() => {
+  mock = jest.spyOn(utils, 'log')
+  mock.mockImplementation(x => x)
+})
+
 afterEach(() => {
+  mock.mockRestore()
   for (const w of watchers) {
     w.close()
   }
 })
+
+const setup = (opts: $Shape<Opts>) => ({
+  watch: 'app',
+  plugins: [],
+  templates: [],
+  ...opts,
+})
+
+const addToWatchers = (opts: $Shape<Opts>) => () => {
+  watchers.push(m(setup(opts)))
+}
 
 test('export function', () => {
   expect(typeof m).toBe('function')
@@ -28,41 +43,34 @@ test('throw error when opts.watch == null', () => {
 test('throw error when opts.plugins !== Array', () => {
   expect(() => {
     // $FlowFixMe
-    m({ watch: 'app', plugins: 'not array' })
+    m(setup({ plugins: 'not array' }))
   }).toThrowErrorMatchingSnapshot()
 })
 
 test('throw error when opts.templates !== Array', () => {
   expect(() => {
     // $FlowFixMe
-    m({ watch: 'app', plugins: [], templates: 'string' })
+    m(setup({ templates: 'string' }))
   }).toThrowErrorMatchingSnapshot()
 })
 
 test('throw error when opts.after !== Array', () => {
   expect(() => {
     // $FlowFixMe
-    m({ watch: 'app', plugins: [], templates: [], afterHooks: 'string' })
+    m(setup({ afterHooks: 'string' }))
   }).toThrowErrorMatchingSnapshot()
 })
 
 test('not throw when plugins,templates == null', () => {
-  const wrap = (opts: $Shape<Opts>) => () => {
-    watchers.push(m({ watch: 'app', ...opts }))
-  }
-  expect(wrap()).not.toThrow()
+  expect(addToWatchers()).not.toThrow()
   // $FlowFixMe
-  expect(wrap({ plugins: null })).not.toThrow()
+  expect(addToWatchers({ plugins: null })).not.toThrow()
   // $FlowFixMe
-  expect(wrap({ templates: null })).not.toThrow()
+  expect(addToWatchers({ templates: null })).not.toThrow()
 })
 
 test('Displayed "start" on the Consle', () => {
-  mock.mockReset()
-  const wrap = (opts: $Shape<Opts>) => () => {
-    watchers.push(m({ watch: 'app', ...opts }))
-  }
-  expect(wrap()).not.toThrow()
+  expect(addToWatchers()).not.toThrow()
   expect(mock.mock.calls.length).toBe(1)
   expect(mock.mock.calls[0][0]).toMatch('start')
 })
