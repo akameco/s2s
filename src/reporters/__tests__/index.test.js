36f88e2ce4c9673f5abdb8ceefd13c65cfd7ab2c
akameco <akameco.t@gmail.com>
2017-09-23 13:14:36 +0900

    ansi program for np

--- src/reporters/__tests__/index.test.js
+++ src/reporters/__tests__/index.test.js
@@ -1,22 +1,18 @@
 // @flow
-import chalk from 'chalk'
 import stripAnsi from 'strip-ansi'
-
-jest.resetModules()
-jest.doMock('chalk', () => new chalk.constructor({ enabled: true }))
-
-// eslint-disable-next-line
 import { formatText, trimAndFormatPath } from '../'
 
 test('trimAndFormatPath', () => {
-  expect(trimAndFormatPath('/src/user/hoge/index.js')).toMatchSnapshot()
+  expect(
+    stripAnsi(trimAndFormatPath('/src/user/hoge/index.js'))
+  ).toMatchSnapshot()
 })
 
 test('formatText when handlerType = s2s', () => {
   const inputPath = 'src/input/input.js'
   const outputPath = 'src/output/output.js'
   const result = formatText('S2S', inputPath, outputPath)
-  expect(result).toMatchSnapshot()
+  // expect(result).toMatchSnapshot()
   expect(stripAnsi(result)).toMatchSnapshot()
 })
 
@@ -24,6 +20,6 @@ test('formatText when handlerType = template', () => {
   const inputPath = 'src/input/input.js'
   const outputPath = 'src/output/output.js'
   const result = formatText('TEMPLATE', inputPath, outputPath)
-  expect(result).toMatchSnapshot()
+  // expect(result).toMatchSnapshot()
   expect(stripAnsi(result)).toMatchSnapshot()
 })
