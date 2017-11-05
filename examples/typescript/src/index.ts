bc0fa47ed7e97177a96c7157f3e1fc888425c626
akameco <akameco.t@gmail.com>
2017-11-06 02:42:59 +0900

    Tweak

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -3,7 +3,5 @@ function greet(name: string) {
 }
 
 for (var i = 1; i < 10; i++) {
-  console.log(
-    i % 15 === 0 ? 'FizzBuzz' : i % 3 === 0 ? 'Fizz' : i % 5 === 0 ? 'Buzz' : i
-  )
+  console.log(i)
 }
