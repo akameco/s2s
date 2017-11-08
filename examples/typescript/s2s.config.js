eef4858fc5cf3308aa9a0b6968929ecfd547f739
akameco <akameco.t@gmail.com>
2017-11-08 12:30:20 +0900

    docs(example/typescirpt): fix example

--- examples/typescript/s2s.config.js
+++ examples/typescript/s2s.config.js
@@ -1,12 +1,9 @@
-const tsHandler = require('s2s-handler-typescript').default
-
 module.exports = {
   watch: './**/*.ts',
   plugins: [
     {
       test: /.*.ts$/,
       plugin: ['fizzbuzz'],
-      handler: tsHandler,
     },
   ],
 }
