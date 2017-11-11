740aec42e6c1c188c9739c1ac274721868970642
akameco <akameco.t@gmail.com>
2017-11-12 08:45:07 +0900

    refactor(s2s-utils): use mjs

--- packages/s2s-utils/src/index.js
+++ packages/s2s-utils/src/index.js
@@ -1,14 +1,14 @@
 // @flow
-const { relative, dirname, extname, normalize } = require('path')
-const slash = require('slash')
-const babelTemplate = require('babel-template')
-const t = require('babel-types')
+import { relative, dirname, extname, normalize } from 'path'
+import slash from 'slash'
+import babelTemplate from 'babel-template'
+import * as t from 'babel-types'
 
-function trimExtension(path /* : string */, ext /* : string */ = '.js') {
+export function trimExtension(path: string, ext: string = '.js') {
   return extname(path) === ext ? path.replace(ext, '') : path
 }
 
-function getImportPath(from /* : string */, to /* : string */) /* : string */ {
+export function getImportPath(from: string, to: string): string {
   const relativePath = slash(relative(dirname(from), to))
   const fomattedPath = trimExtension(relativePath)
   if (!/^\.\.?/.test(fomattedPath)) {
@@ -17,13 +17,14 @@ function getImportPath(from /* : string */, to /* : string */) /* : string */ {
   return fomattedPath
 }
 
-function template(code /* : string */, plugins /* :?string[] */ = ['flow']) {
+export function template(code: string, plugins: ?(string[]) = ['flow']) {
   return babelTemplate(code, { sourceType: 'module', plugins })
 }
 
-function inheritsOpts() {
+export function inheritsOpts() {
   return {
-    manipulateOptions(opts /* : Object */, parserOpts /* : Object */) {
+    // eslint-disable-next-line flowtype/no-weak-types
+    manipulateOptions(opts: Object, parserOpts: Object) {
       ;['flow', 'objectRestSpread'].forEach(plugin => {
         parserOpts.plugins.push(plugin)
       })
@@ -31,16 +32,12 @@ function inheritsOpts() {
   }
 }
 
-function getParentDirName(path /* : string */) {
+export function getParentDirName(path: string) {
   const parentPath = normalize(dirname(path)).split('/')
   return parentPath[parentPath.length - 1]
 }
 
-function typeImport(
-  local /*: string*/,
-  imported /*: string*/,
-  source /*: string */
-) {
+export function typeImport(local: string, imported: string, source: string) {
   const im = t.importDeclaration(
     [t.importSpecifier(t.identifier(local), t.identifier(imported))],
     t.stringLiteral(source)
@@ -49,16 +46,16 @@ function typeImport(
   return im
 }
 
-function defaultImport(local /*: string */, source /*: string*/) {
+export function defaultImport(local: string, source: string) {
   return t.importDeclaration(
     [t.importDefaultSpecifier(t.identifier(local))],
     t.stringLiteral(source)
   )
 }
 
-function createImportDeclaration(
-  locals /*: string[] | string */,
-  source /*: string */
+export function createImportDeclaration(
+  locals: string[] | string,
+  source: string
 ) {
   const finalLocals = [].concat(locals)
   const specifiers = finalLocals.map(local => {
@@ -67,11 +64,3 @@ function createImportDeclaration(
   })
   return t.importDeclaration(specifiers, t.stringLiteral(source))
 }
-
-exports.defaultImport = defaultImport
-exports.getImportPath = getImportPath
-exports.template = template
-exports.inheritsOpts = inheritsOpts
-exports.getParentDirName = getParentDirName
-exports.typeImport = typeImport
-exports.createImportDeclaration = createImportDeclaration
