1686cd82e753eb2e15954021b6a161410a1426f2
akameco <akameco.t@gmail.com>
2017-11-22 10:36:38 +0900

    feat(reducer-case-creater): create object with action type

--- packages/s2s-helper-get-action-obj/src/index.test.js
+++ packages/s2s-helper-get-action-obj/src/index.test.js
@@ -1,5 +1,5 @@
 // @flow
-import helper from '.'
+import helper, { getTypeProperty, getAllTypeProperty } from '.'
 
 test('return [] when empty object', () => {
   const fixture = `
@@ -37,4 +37,69 @@ test('return [] when Actions not exist', () => {
 export const Maction = {Increment}
 `
   expect(helper(fixture)).toEqual([])
+})
+
+test('getTypeProperty', () => {
+  const fixture = `
+    export type SetCount = {
+      type: 'SET_COUNT',
+      count: number,
+    }
+  `
+  const target = 'SetCount'
+  expect(getTypeProperty(fixture, target)).toEqual(['type', 'count'])
+})
+
+test('getTypeProperty when empty type', () => {
+  const fixture = `
+    export type SetCount = { }
+  `
+  const target = 'SetCount'
+  expect(getTypeProperty(fixture, target)).toEqual([])
+})
+
+test('getTypeProperty when no target', () => {
+  const fixture = `
+    export type SetCount = { }
+  `
+  const target = 'Woooooo'
+  expect(getTypeProperty(fixture, target)).toEqual([])
+})
+
+test('getTypeProperty when no object type', () => {
+  const fixture = `
+    export type SetCount = number
+  `
+  const target = 'SetCount'
+  expect(getTypeProperty(fixture, target)).toEqual([])
+})
+
+test('getAllTypeProperty', () => {
+  const fixture = `
+    export type SetCount = {
+      type: 'SET_COUNT',
+      count: number,
+    }
+  `
+  expect(getAllTypeProperty(fixture)).toEqual({ SetCount: ['type', 'count'] })
+})
+
+test('getAllTypeProperty when some types', () => {
+  const fixture = `
+    export type SetCount = {
+      type: 'SET_COUNT',
+      count: number,
+    }
+
+    export type Increment = {
+      type: 'INCREMENT',
+    }
+
+    type Opps = string
+  `
+
+  expect(getAllTypeProperty(fixture)).toEqual({
+    Increment: ['type'],
+    SetCount: ['type', 'count'],
+  })
 })
