579659a5f065d29275cff4ff0354b1bc5fc2ab4f
kamijin_fanta <kamijin@live.jp>
2017-12-08 10:57:09 +0900

    change test design
    - delete fixturesEnv
    - change to using beforeEach/afterEach

--- packages/s2s-hook-tslint/src/index.test.js
+++ packages/s2s-hook-tslint/src/index.test.js
@@ -3,95 +3,70 @@ import path from 'path'
 import fs from 'fs'
 import tslintHook from '.'
 
-// load fixtures, run tet, delete tmpfile
-function fixturesEnv(
-  location: string,
-  name: string,
-  fn: ({
-    actualFile: string,
-    expectedFile: string,
-    tmpPath: string,
-    tslintPath: string,
-  }) => void
-) {
+// load test files
+function load(location: string) {
   const files = ['actual.ts', 'expected.ts', 'tmp.ts', 'tslint.json']
   const [actualPath, expectedPath, tmpPath, tslintPath] = files.map(file =>
-    path.resolve(location, name, file)
+    path.resolve(location, file)
   )
 
-  try {
-    // $FlowFixMe
-    fs.copyFileSync(actualPath, tmpPath) // Node v8.5.0~
+  const [actualFile, expectedFile] = [actualPath, expectedPath].map(file =>
+    fs.readFileSync(file, 'utf8')
+  )
 
-    const [actualFile, expectedFile] = [actualPath, expectedPath].map(file =>
-      fs.readFileSync(file, 'utf8')
-    )
-
-    fn({ actualFile, expectedFile, tmpPath, tslintPath })
-  } finally {
-    fs.unlinkSync(tmpPath)
-  }
+  return { actualFile, actualPath, expectedFile, tmpPath, tslintPath }
 }
 
-const loc = path.resolve(__dirname, '../__fixtures__')
-
-let errorSpy
-
-const fn = x => x
+const loc = path.resolve(__dirname, '../__fixtures__/basic')
+const { actualFile, actualPath, expectedFile, tmpPath, tslintPath } = load(loc)
 
 jest.useFakeTimers()
+
+let errorSpy
+const fn = x => x
 
 beforeEach(() => {
   jest.runAllTimers()
   errorSpy = jest.spyOn(console, 'error').mockImplementation(fn)
+  // $FlowFixMe
+  fs.copyFileSync(actualPath, tmpPath) // Node v8.5.0~
 })
 
 afterEach(() => {
   errorSpy.mockRestore()
+  fs.unlinkSync(tmpPath)
 })
 
 test('basic', () => {
-  fixturesEnv(loc, 'basic', ops => {
-    const { actualFile, expectedFile, tmpPath, tslintPath } = ops
-    const hook = tslintHook({
-      test: /\.ts$/,
-      lintConfig: tslintPath,
-    })
-    const res = hook(actualFile, tmpPath)
-    expect(res).toBe(expectedFile)
+  const hook = tslintHook({
+    test: /\.ts$/,
+    lintConfig: tslintPath,
   })
+  const res = hook(actualFile, tmpPath)
+  expect(res).toBe(expectedFile)
 })
 
 test('default options', () => {
-  fixturesEnv(loc, 'basic', ops => {
-    const { actualFile, expectedFile, tmpPath } = ops
-    const hook = tslintHook()
-    const res = hook(actualFile, tmpPath)
-    expect(res).toBe(expectedFile)
-  })
+  const hook = tslintHook()
+  const res = hook(actualFile, tmpPath)
+  expect(res).toBe(expectedFile)
 })
 
 test('lintConfig option is incorrect', () => {
-  fixturesEnv(loc, 'basic', ops => {
-    const { actualFile, tmpPath, tslintPath } = ops
-    const hook = tslintHook({
-      test: /\.ts$/,
-      lintConfig: `${tslintPath}-bad`,
-    })
-    const res = hook(actualFile, tmpPath)
-    expect(res).toBe(actualFile) // ignored error
-    expect(errorSpy.mock.calls[0][0]).toBe('tslint error')
+  const hook = tslintHook({
+    test: /\.ts$/,
+    lintConfig: `${tslintPath}-bad`,
   })
+  const res = hook(actualFile, tmpPath)
+  expect(res).toBe(actualFile) // ignored error
+  expect(errorSpy.mock.calls[0][0]).toBe('tslint error')
 })
 
 test('not match filepath', () => {
-  fixturesEnv(loc, 'basic', ops => {
-    const { actualFile, tmpPath, tslintPath } = ops
-    const hook = tslintHook({
-      test: /\.html$/,
-      lintConfig: `${tslintPath}`,
-    })
-    const res = hook(actualFile, tmpPath)
-    expect(res).toBe(actualFile) // input == output
+  const hook = tslintHook({
+    test: /\.html$/,
+    lintConfig: `${tslintPath}`,
   })
+  const res = hook(actualFile, tmpPath)
+  expect(res).toBe(actualFile) // input == output
 })
