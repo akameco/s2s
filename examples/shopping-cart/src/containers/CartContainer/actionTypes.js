c5538a3971b929195277fb9a19e56af879c01783
akameco <akameco.t@gmail.com>
2017-12-13 20:28:53 +0900

    feat(s2s-action-types): change order (#119)
    * feat(s2s-action-types): change order
    
    BREAKING CHANGE: change order
    
    * chore(example): update example for s2s-action-types
    
    * docs(s2s-action-types): update readme
    
    * test(s2s-action-types): update snapshot

--- examples/shopping-cart/src/containers/CartContainer/actionTypes.js
+++ examples/shopping-cart/src/containers/CartContainer/actionTypes.js
@@ -1,18 +1,11 @@
 // @flow
 import type { Cart } from '../../types'
 
-export const CHECKOUT_REQUEST: 'CartContainer/CHECKOUT_REQUEST' =
-  'CartContainer/CHECKOUT_REQUEST'
-export const CHECKOUT_SUCCESS: 'CartContainer/CHECKOUT_SUCCESS' =
-  'CartContainer/CHECKOUT_SUCCESS'
-export const ADD_TO_CART: 'CartContainer/ADD_TO_CART' =
-  'CartContainer/ADD_TO_CART'
-
-export const Actions = {
-  CHECKOUT_REQUEST,
-  CHECKOUT_SUCCESS,
-  ADD_TO_CART,
-}
+export type Action =
+  | CheckoutRequest
+  | CheckoutSuccess
+  | CheckoutFailure
+  | AddToCart
 
 export type CheckoutRequest = {
   type: typeof CHECKOUT_REQUEST,
@@ -23,9 +16,27 @@ export type CheckoutSuccess = {
   cart: Cart,
 }
 
+export type CheckoutFailure = {
+  type: typeof CHECKOUT_FAILURE,
+}
+
 export type AddToCart = {
   type: typeof ADD_TO_CART,
   productId: number,
 }
 
-export type Action = CheckoutRequest | CheckoutSuccess | AddToCart
+export const CHECKOUT_REQUEST: 'CartContainer/CHECKOUT_REQUEST' =
+  'CartContainer/CHECKOUT_REQUEST'
+export const CHECKOUT_SUCCESS: 'CartContainer/CHECKOUT_SUCCESS' =
+  'CartContainer/CHECKOUT_SUCCESS'
+export const CHECKOUT_FAILURE: 'CartContainer/CHECKOUT_FAILURE' =
+  'CartContainer/CHECKOUT_FAILURE'
+export const ADD_TO_CART: 'CartContainer/ADD_TO_CART' =
+  'CartContainer/ADD_TO_CART'
+
+export const Actions = {
+  CHECKOUT_REQUEST,
+  CHECKOUT_SUCCESS,
+  CHECKOUT_FAILURE,
+  ADD_TO_CART,
+}
--- packages/babel-plugin-s2s-action-types/readme.md
+++ packages/babel-plugin-s2s-action-types/readme.md
@@ -20,17 +20,17 @@ export type Action = Increment
 
 ```js
 // @flow
-export const INCREMENT: 'app/counter/INCREMENT' = 'app/counter/INCREMENT'
-
-export const Actions = {
-  INCREMENT,
-}
+export type Action = Increment
 
 export type Increment = {
   type: typeof INCREMENT,
 }
 
-export type Action = Increment
+export const INCREMENT: 'app/counter/INCREMENT' = 'app/counter/INCREMENT'
+
+export const Actions = {
+  INCREMENT,
+}
 ```
 
 ### Request/Success/Failure pattern
@@ -44,6 +44,19 @@ export type Action = FetchRequest
 #### Out:
 
 ```js
+// @flow
+export type Action = FetchRequest | FetchSuccess | FetchFailure
+
+export type FetchRequest = {
+  type: typeof FETCH_REQUEST,
+}
+export type FetchSuccess = {
+  type: typeof FETCH_SUCCESS,
+}
+export type FetchFailure = {
+  type: typeof FETCH_FAILURE,
+}
+
 export const FETCH_REQUEST: 'app/counter/FETCH_REQUEST' =
   'app/counter/FETCH_REQUEST'
 export const FETCH_SUCCESS: 'app/counter/FETCH_SUCCESS' =
@@ -56,16 +69,4 @@ export const Actions = {
   FETCH_SUCCESS,
   FETCH_FAILURE,
 }
-
-export type FetchRequest = {
-  type: typeof FETCH_REQUEST,
-}
-export type FetchSuccess = {
-  type: typeof FETCH_SUCCESS,
-}
-export type FetchFailure = {
-  type: typeof FETCH_FAILURE,
-}
-
-export type Action = FetchRequest | FetchSuccess | FetchFailure
 ```
