46f79bbaa8f148dc64d636911dac5a7ef6fdc37f
akameco <akameco.t@gmail.com>
2017-12-14 03:16:20 +0900

    chore(examples/shopping-cart): fix flow error

--- examples/shopping-cart/flow-typed/global.js
+++ examples/shopping-cart/flow-typed/global.js
@@ -1,1 +1,13 @@
 type Exact<T> = T & $Shape<T>
+
+declare module 's2s-plugins-redux' {
+  declare module.exports: any => any;
+}
+
+declare module 'enzyme-to-json' {
+  declare module.exports: any => Object;
+}
+
+declare module 'redux-thunk' {
+  declare module.exports: any => any;
+}
