c348dcd4d249e8aefe62fd2d30cb4df172ddec5d
kamijin_fanta <kamijin@live.jp>
2017-12-15 16:24:28 +0900

    [s2s-cli] change hook api.
    - passed to hook from inputPath to outputPath.

--- packages/s2s-cli/src/hooks/index.js
+++ packages/s2s-cli/src/hooks/index.js
@@ -3,11 +3,11 @@ import type { Path, AfterHook } from 'types'
 
 export default function runHooks(
   code: string,
-  input: Path,
+  output: Path,
   hooks: AfterHook[] = []
 ) {
   const result = hooks.reduce((prev, next) => {
-    return next(prev, input)
+    return next(prev, output)
   }, code)
 
   return result
