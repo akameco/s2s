3cb421a1ffecb0b7ef90f6c37fa64ac90d3c3105
akameco <akameco.t@gmail.com>
2018-01-22 17:18:19 +0900

    chore(sample): fix type

--- examples/shopping-cart/flow-typed/global.js
+++ examples/shopping-cart/flow-typed/global.js
@@ -1,9 +1,5 @@
 type Exact<T> = T & $Shape<T>
 
-declare module 's2s-plugins-redux' {
-  declare module.exports: any => any;
-}
-
 declare module 'enzyme-to-json' {
   declare module.exports: any => Object;
 }
