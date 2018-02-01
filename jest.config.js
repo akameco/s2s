69e5b0598df1a48888ffd624ce88af3814b7a831
akameco <akameco.t@gmail.com>
2018-02-01 22:00:24 +0900

    chore(*): fix ignore path

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,17 @@
 
 module.exports = {
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/', '/.*/lib/', '/templates/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/.*/lib/',
+    '/s2s-templates-redux/templates/',
+  ],
   watchPathIgnorePatterns: ['/fixtures/copy*'],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/fixtures/',
     '/helpers/',
-    '/templates/',
+    '/s2s-templates-redux/templates',
   ],
   snapshotSerializers: [
     '<rootDir>/node_modules/pretty-format/build/plugins/convert_ansi.js',
