316f5c8f43e6cfb311ef98d0531b10828cc729dc
akameco <akameco.t@gmail.com>
2018-03-20 10:33:48 +0900

    chore(*): rename package name

--- examples/react-redux-type-d/s2s.config.js
+++ examples/react-redux-type-d/s2s.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const actionConstants = require('babel-plugin-s2s-d-action-constants').default
 const actionTypes = require('babel-plugin-s2s-d-action-types').default
 const actionCreater = require('babel-plugin-s2s-d-action-creater').default
-const actionTest = require('babel-plugin-s2s-d-action-test').default
+const actionTest = require('babel-plugin-s2s-d-action-tests').default
 
 const actionTypesMatch = /actions\/.*\/types.js/
 
