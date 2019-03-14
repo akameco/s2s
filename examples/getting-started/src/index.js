d16f0aa2156c8111a234985f3d3eaf3602a1ba32
akameco <akameco.t@gmail.com>
2019-03-14 23:54:31 +0900

    chore(deps): upgrade prettier

--- examples/getting-started/src/index.js
+++ examples/getting-started/src/index.js
@@ -3,7 +3,7 @@
  * Welcome to s2s https://github.com/akameco/s2s.
  * This example is very small. However Useful :)
  * This plugin is that initializing variable with flow information.
-*/
+ */
 
 type User = { id: number, name: string }
 // type `var user: User` and save!
